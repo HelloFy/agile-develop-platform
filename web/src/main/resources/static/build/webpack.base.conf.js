e99e29256d7ec1673c3ad64941aa2637b554ef51
FeiYue <916073811@qq.com>
2017-03-17 00:52:20 +0800

    modify ui

--- web/src/main/resources/static/build/webpack.base.conf.js
+++ web/src/main/resources/static/build/webpack.base.conf.js
@@ -70,6 +70,10 @@ module.exports = {
             {
                 test: /\.css$/,
                 loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-withimg-loader'
             }
         ]
     }
--- web/src/main/resources/static/src/js/index.js
+++ web/src/main/resources/static/src/js/index.js
@@ -1,2 +1,4 @@
 require('./semantic/semantic.min.js')
 require('../css/semantic/dist/semantic.min.css')
+require('../template/index.html')
+require('../css/index.css')
